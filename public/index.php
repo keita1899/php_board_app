f3736412101e14985bb57acf06113f7383883704
keita1899 <saboten18k@gmail.com>
2025-04-29 21:33:03 +0900

    feat: ログアウト機能実装

--- public/index.php
+++ public/index.php
@@ -1,3 +1,28 @@
 <?php
-// publicディレクトリの動作確認用
-echo "Hello, World!"; 
+session_start();
+
+echo "Hello, World!";
+
+if (!isset($_SESSION['user_id'])) {
+  echo "ログインしてください";
+} else {
+  echo "ログインしました";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php if (isset($_SESSION['user_id'])): ?>
+    <form action="/logout.php" method="post">
+      <input type="submit" value="ログアウト">
+    </form>
+  <?php endif; ?>
+</body>
+</html>
+
