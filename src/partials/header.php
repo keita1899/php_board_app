59feef911c0b8eb749ab45e83f7b28cc5a1ea818
keita1899 <saboten18k@gmail.com>
2025-05-12 21:54:07 +0900

    refactor: ログインしているかどうかを関数で修正

--- src/partials/header.php
+++ src/partials/header.php
@@ -1,8 +1,12 @@
+<?php
+require_once __DIR__ . '/../lib/auth.php';
+?>
+
 <header>
   <div class="header-content">
     <h1>掲示板アプリ</h1>
     <nav class="header-nav">
-      <?php if (isset($_SESSION['user_id'])): ?>
+      <?php if (is_logged_in()): ?>
         <form action="/logout.php" method="post" class="nav-form">
           <input type="hidden" name="csrf_token" value="<?= htmlspecialchars(generate_csrf_token()) ?>">
           <input type="submit" value="ログアウト" class="nav-button">
